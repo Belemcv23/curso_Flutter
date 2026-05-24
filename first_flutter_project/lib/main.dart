a7755444a0e264c15aaa70f1f75b5a8bf457a3a2
Maria Belem Carrera Velasco <l22350634@tuxtepec.tecnm>
2026-05-23 19:27:18 -0600

    sección 3:creación de clases para organizar el código

--- first_flutter_project/lib/main.dart
+++ first_flutter_project/lib/main.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    Text('This is a text.')
+    MaterialApp(
+      title:"My flutter app",
+      home:Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar:AppBar(
+          title: Text('title'),
+        ),
+        body:Center(
+          child: Text('This is a text.', textDirection: TextDirection.ltr,)
+        )
+      ),
+    ),
   );
 }
